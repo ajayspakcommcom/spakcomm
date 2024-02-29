2a5ad0f52619354d5f5ce861747397e7aa5b2df3
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-02-29 11:37:18 +0530

    latest changes

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {id: number, name: string };
+type Data = { id: number, name: string };
 
-const data: Data[] = [{id:1, name:'Ram'}, {id:2, name:'Manjeet'}, {id:3, name:'Sujeet'}, {id:1, name:'Kamlesh'}];
+const data: Data[] = [{ id: 1, name: 'Ram' }, { id: 2, name: 'Manjeet' }, { id: 3, name: 'Sujeet' }, { id: 1, name: 'Kamlesh' }];
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data[]>) {
   res.status(200).json(data);
