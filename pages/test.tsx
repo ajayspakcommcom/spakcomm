d6e08b4116febab02d194c07c03699ebf4527e75
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-05-22 18:10:06 +0530

    changes

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-class Base {
-    private x = 0;
-}
-
 const Index: React.FC = () => {
+
+    let first = [1, 2];
+    let second = [3, 4];
+    let bothPlus = [0, ...first, ...second, 5];
+
+
+    console.log(bothPlus);
 
     return (
         <>
+
         </>
     );
 };
