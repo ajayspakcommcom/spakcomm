fd74cd3c70ccc59ecf0979b85c0c195f80e23eee
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-06-06 14:33:38 +0530

    changes

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import dynamic from 'next/dynamic';
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-const Index: React.FC = () => {
+const BubbleChart = () => {
 
-    let first = [1, 2];
-    let second = [3, 4];
-    let bothPlus = [0, ...first, ...second, 5];
+    const series = [{
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }];
 
+    const options = {
+        chart: {
+            height: 350,
 
-    console.log(bothPlus);
+            zoom: {
+                enabled: false
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        title: {
+            text: 'Product Trends by Month',
+            align: 'left'
+        },
+        grid: {
+            row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+            },
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+        }
+    };
+
 
     return (
         <>
-
+            <div id="chart">
+                <ReactApexChart options={options} series={series} type="line" height={350} />
+            </div>
         </>
     );
 };
 
-export default Index;
+export default BubbleChart;
